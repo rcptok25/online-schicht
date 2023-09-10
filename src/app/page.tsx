8d4ebeded32e029614831fafe89712d26a3b346e
rtok <rtok@yukatech.com>
2023-09-10 18:38:26 +0300

    feat: project has been created.

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+import {  Flex ,Text,} from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <Flex color='white' style={{width:"100%",height:"100vh",alignItems:"center",justifyContent:"center"}}>
+    <Text> Hello  The Universe !</Text>
+  </Flex>
+  )
+}
